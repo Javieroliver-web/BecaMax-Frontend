9490e6aef7bb7a8b75e7e7a90a8795385d93f85c
Javieroliver-web <javip200555@gmail.com>
2026-03-22 19:17:44 +0100

    Configurar keys de Supabase

--- js/supabase.js
+++ js/supabase.js
@@ -5,8 +5,8 @@
 //  3. Copia la "Project URL" y la "anon public" key aquí
 // ============================================================
 
-const SUPABASE_URL = 'https://TU_PROYECTO.supabase.co';
-const SUPABASE_ANON_KEY = 'TU_ANON_KEY_AQUI';
+const SUPABASE_URL = 'https://aklieayhnjnhikmggpiv.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_09neP8LoEfPp96xCoxKEwA_X9gk5cxc';
 
 const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
